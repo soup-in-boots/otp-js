33c15036564013260af9d3ffef331fc036c218fc
Zachary Hueras <zhueras@amdtelemedicine.com>
2019-04-03 21:24:25 -0400

    More for the example

---
+++ example/index.js
@@ -0,0 +1,25 @@
+const debug         = require('debug');
+const {create_node} = require('../lib/node');
+const number_server = require('./number_server');
+const gen_server    = require('../lib/otp/gen_server');
+
+const {OTPNode} = create_node(null);
+
+const node = new OTPNode();
+
+const log = debug('open-telecom:example');
+
+node.spawn(async function(ctx) {
+    const result    = await number_server.start_link(ctx);
+    const {ok, pid} = result;
+
+    log('result : %o', result);
+
+    await gen_server.call(ctx, pid, 'add');
+    gen_server.cast(ctx, pid, 'add');
+    ctx.send(pid, 3);
+    ctx.send(pid, 7);
+
+    const final = await gen_server.call(ctx, pid, 'get');
+    log('final : %o', final);
+});
--- example/number_server.js
+++ example/number_server.js
@@ -1,53 +1,79 @@
-const {createNode} = require('../lib');
-const {OTPNode}    = createNode(null);
+const {create_node} = require('../lib');
+const debug         = require('debug');
+const {OTPNode}     = create_node(null);
 const node         = new OTPNode();
 
 const gen_server = require('../lib/otp/gen_server');
 
+const log = debug('open-telecom:example:number-server')
+
 async function start(ctx) {
-    return gen_server.start(ctx, {init, handleCall, handleCast, handleInfo});
+    return gen_server.start(ctx, {init, handle_call, handle_cast, handle_info});
 }
 
 async function start_link(ctx) {
-    return gen_server.start_link(ctx, {init, handleCall, handleCast, handleInfo});
+    return gen_server.start_link(ctx, {init, handle_call, handle_cast, handle_info});
 }
 
 function init(ctx) {
     const ok    = true;
     const state = 0;
 
+    log('init');
+
     return {ok, state};
 }
 
-function handleCall(ctx, call, from, state) {
+function handle_call(ctx, call, from, state) {
+    log('handle_call');
     switch (call) {
-        case 'add':
-            const newState = state + 1;
-            return {reply: newState, state: newState};
-        case 'subtract':
-            const newState = state - 1;
+        case 'add': {
+            const new_state = state + 1;
+            return {reply: new_state, state: new_state};
+        }
+        case 'subtract': {
+            const new_state = state - 1;
             return {reply, state: state};
-        default:
+        }
+        case 'get': {
+            return {reply: state, state};
+        }
+        default: {
             return {state};
+        }
     }
 }
 
-function handleCast(ctx, cast, state) {
+function handle_cast(ctx, cast, state) {
+    log('handle_cast');
     switch (cast) {
-        case 'add':
-            const newState = state + 1;
-            return {state: newState};
-        case 'subtract':
-            const newState = state - 1;
+        case 'add': {
+            const new_state = state + 1;
+            return {state: new_state};
+        }
+        case 'subtract': {
+            const new_state = state - 1;
             return {state: state};
-        default:
+        }
+        default: {
             return {state};
+        }
     }
 }
 
-function handleInfo(ctx, info, state) {
+function handle_info(ctx, info, state) {
+    log('handle_info');
     if (typeof info == 'number')
         return {state: state * info};
 
     return {state};
 }
+
+module.exports = {
+    start,
+    start_link,
+    handle_info,
+    handle_call,
+    handle_cast,
+    init,
+};
